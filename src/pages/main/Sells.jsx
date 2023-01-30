6f42f4b53ceceb3468f0266bf90de1d91e28c617
Beka <bekzot.abdyldayev@gmailcom>
2023-01-30 12:45:07 +0600

    modified:   src/actions/itemsActions.js 	modified:   src/components/AddEditDialog.jsx 	modified:   src/pages/main/Sells.jsx

--- src/pages/main/Sells.jsx
+++ src/pages/main/Sells.jsx
@@ -2,41 +2,65 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { SideBar } from '../../components/SideBar';
 import { DataGrid } from '@mui/x-data-grid';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchOrders } from '../../actions/itemsActions';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'id', headerName: 'OrderID', width: 70 },
+  { field: 'Address', headerName: 'Address', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    field: 'Items',
+    headerName: 'Items',
+    width: 790,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
+    field: 'createdAt',
+    headerName: 'createdAt',
+    width: 260,
+  },
+  {
+    field: 'Status',
+    headerName: 'Status',
+    sortable: true,
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 export const Sells = () => {
+  const dispatch = useDispatch();
+  const [orders, setOrders] = React.useState([]);
+  const [rows, setRows] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
+  let items = useSelector((state) => state.items.items);
+
+  useEffect(() => {
+    dispatch(fetchOrders(setOrders));
+  }, []);
+
+  useEffect(() => {
+    const newRows = orders.map((order) => {
+      return {
+        id: order._id,
+        Address: order.address,
+        Items: order.items
+          .map((item) => {
+            return `${item._id} size: ${item.size} quantity: ${item.quantity}`;
+          })
+          .join(', '),
+        createdAt: order.createdAt,
+        Status: order.status,
+      };
+    });
+    setRows(newRows);
+    console.log(orders);
+  }, [orders]);
+
+  const updateCurrentItem = (e) => {
+    setSelected(items.filter((item) => item._id === e.row.id));
+    console.log('items', items);
+  };
+
   return (
     <div>
       <Box sx={{ display: 'flex' }}>
@@ -49,9 +73,18 @@ export const Sells = () => {
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
-          checkboxSelection
+          onRowClick={updateCurrentItem}
         />
       </div>
+
+      {selected.map((item) => {
+        return (
+          <div>
+            <h1>{item.title}</h1>
+            <h1>{item.price}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 };
